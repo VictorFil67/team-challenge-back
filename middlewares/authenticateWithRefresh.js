88e10887b5cebb50215a159560a2e4b555c514ca
VictorFil67 <victorfilinovych@gmail.com>
2024-09-11 21:13:30 +0300

    updated authenticate with refresh

--- middlewares/authenticateWithRefresh.js
+++ middlewares/authenticateWithRefresh.js
@@ -7,7 +7,6 @@ const { JWT_SECRET } = process.env;
 
 const authenticateWithRefresh = async (req, res, next) => {
   const { authorization } = req.headers;
-  // console.log(authorization);
   if (!authorization) {
     return next(HttpError(401));
   }
